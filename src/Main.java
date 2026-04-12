e94267fd65b5bbe1acada0a7eca23933f8d25e6c
EliaSpalla <eliagianfranceschi@gmail.com>
2026-04-12 17:02:42 +0200

    progetto allo stato BETA, completato Main e BST

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         // Popola l'albero con dati predefiniti
         inserimentoElementiBase();
 
-        IO.print("premere ENTER per passare al menu");
+        System.out.print("premere ENTER per passare al menu");
         sc.nextLine();
 
         menu();
@@ -22,10 +22,11 @@ public class Main {
 
         while (scelta != 0) {
             do {
-                IO.println("--- Menu ---\n");
-                IO.println("0. exit\n1. diagnostica\n2. aggiungi un elemento\n3. ricerca di un elemento\n4. stampa catalogo");
+                System.out.println("--- Menu ---\n");
+                System.out.println("0. exit\n1. diagnostica\n2. aggiungi un elemento\n3. ricerca di un elemento\n4. stampa catalogo");
 
                 scelta = sc.nextInt();
+                pulisciTerminale(); // funziona solo su terminale
                 switch (scelta) {
                     case 1:
                         diagnostica(); // Esegue test di controllo sul BST
@@ -42,15 +43,15 @@ public class Main {
                     default:
                         break;
                 }
-                pulisciTerminale(); // funziona solo su terminale
             } while (scelta < 0 || scelta > 4);
+
         }
     }
 
     //Chiede all'utente l'ID da cercare e richiama il metodo di ricerca
     public static void ricercaElemento() {
         Scanner sc = new Scanner(System.in);
-        IO.print("id da cercare: ");
+        System.out.print("id da cercare: ");
         int id = sc.nextInt();
         ricerca(id);
     }
@@ -60,7 +61,7 @@ public class Main {
     //Uso del polimorfismo nella creazione delle istanze.
     public static void inserimentoNuovoElemento() {
         Scanner sc = new Scanner(System.in);
-        IO.println("quale elemento si desidera aggiungere?\n1. libro\n2. film\n3. videogioco");
+        System.out.println("quale elemento si desidera aggiungere?\n1. libro\n2. film\n3. videogioco");
 
         int scelta = -1;
         while (scelta < 1 || scelta > 3) {
@@ -68,12 +69,12 @@ public class Main {
         }
 
         sc.nextLine(); // Svuota il buffer (non so che significa ma va fatto, non è il primo progetto in cui provo a capi)
-        IO.println("Inserisci i dati per il nuovo elemento: ");
-        IO.print("titolo: ");
+        System.out.println("Inserisci i dati per il nuovo elemento: ");
+        System.out.print("titolo: ");
         String titolo = sc.nextLine();
-        IO.print("anno pubblicazione: ");
+        System.out.print("anno pubblicazione: ");
         int annoPubblicazione = sc.nextInt();
-        IO.print("id: ");
+        System.out.print("id: ");
         int id = sc.nextInt();
 
         sc.nextLine(); // Svuota di nuovo il buffer
@@ -81,15 +82,15 @@ public class Main {
 
         // In base alla scelta, istanzia la sottoclasse specifica e la aggiunge al BST
         if (scelta == 1) {
-            IO.print("autore: ");
+            System.out.print("autore: ");
             parametro = sc.nextLine();
             catalogo.inserisci(new Libro(titolo, annoPubblicazione, id, parametro));
         } else if (scelta == 2) {
-            IO.print("regista: ");
+            System.out.print("regista: ");
             parametro = sc.nextLine();
             catalogo.inserisci(new Film(titolo, annoPubblicazione, id, parametro));
         } else {
-            IO.print("piattaforma: ");
+            System.out.print("piattaforma: ");
             parametro = sc.nextLine();
             catalogo.inserisci(new Videogioco(titolo, annoPubblicazione, id, parametro));
         }
@@ -106,7 +107,7 @@ public class Main {
 
     //Popola il catalogo con 10 elementi
     public static void inserimentoElementiBase() {
-        IO.println("--- Inserimento elementi base nel BST ---");
+        System.out.println("--- Inserimento elementi base nel BST ---");
 
         // Array polimorfico contenente diversi tipi di media
         ElementoCatalogo[] nuoviElementi = {
@@ -124,23 +125,24 @@ public class Main {
 
         // Ciclo per inserire ogni elemento dell'array nell'albero binario
         for (ElementoCatalogo e : nuoviElementi) {
-            IO.println(e.toString());
+            System.out.println(e.toString());
             catalogo.inserisci(e);
         }
 
-        IO.println("\nsono stati inseriti con successo: " + nuoviElementi.length + " elementi");
+        System.out.println("\nsono stati inseriti con successo: " + nuoviElementi.length + " elementi");
     }
 
     //Esegue qualche test
     public static void diagnostica() {
-        IO.println("--- Diagnostica ---\n");
+        System.out.println("--- Diagnostica ---\n");
 
         // Test 1: La stampa in ordine deve mostrare gli ID dal più piccolo al più grande
-        IO.println("Visualizzazione ordinata per ID:");
+        System.out.println("Visualizzazione ordinata per ID:");
         catalogo.stampaInOrdine();
 
         // Test 2: Verifica il funzionamento della ricerca per un ID noto
-        IO.println("\n--- Test di Ricerca ---");
+        System.out.println("\n--- Test di Ricerca ---");
+
         int idDaCercare = 350;
         ElementoCatalogo finto = new ElementoCatalogo("", 0, idDaCercare);
         ricerca(finto);
@@ -160,7 +162,7 @@ public class Main {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
                 // Sequenza escape ANSI per sistemi basati su Unix (Linux, macOS)
-                IO.print("\033[H\033[2J");
+                System.out.print("\033[H\033[2J");
                 System.out.flush();
             }
         } catch (Exception e) {
